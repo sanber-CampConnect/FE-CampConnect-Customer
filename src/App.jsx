e0539f6f155fc899075967327677b72d135bcaeb
Muhammad Ricky Zakaria <132816291+MRickyyZ@users.noreply.github.com>
2024-06-05 22:41:58 +0800

    add register page (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login.jsx";
 import Profile from "./pages/profile/Profile.jsx";
+import Register from './pages/auth/Register.jsx';
 function App() {
 
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
                 <Routes>
                     <Route path="/auth/login" element={<Login />} />
+                    <Route path="/auth/register" element={<Register />} />
                     <Route path="/profile" element={<Profile />} />
                 </Routes>
             </BrowserRouter>
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -6,11 +6,16 @@ import IconEmail from "../../assets/icons/iconEmail.png";
 import IconLock from "../../assets/icons/iconLock.png";
 import ImgPlaceholder from "../../assets/placeholder.png";
 
-const Login = () => {
+const Register = () => {
     const [passwordVisible, setPasswordVisible] = useState(false);
+    const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
     const togglePasswordVisibility = () => {
         setPasswordVisible(!passwordVisible);
+    };
+
+    const toggleConfirmPasswordVisibility = () => {
+        setConfirmPasswordVisible(!confirmPasswordVisible);
     };
 
     return (
@@ -27,8 +32,8 @@ const Login = () => {
                     />
                 </div>
             </div>
-            <div className="w-full max-w-xl mx-auto mr-10 p-8 xl:w-1/2">
-                <div className="text-left -ml-60 -mt-16 xl:text-left">
+            <div className="w-full max-w-xl mx-auto p-8 xl:w-1/2">
+                <div className="text-center xl:text-left">
                     <img
                         src={Logo}
                         alt="logo"
@@ -36,18 +41,13 @@ const Login = () => {
                     />
                 </div>
                 <div className="font-poppins text-left mt-6 -ml-10">
-                    <h1 className="text-xl font-semibold xl:hidden block">
-                        Sign in to
-                    </h1>
-                    <p className="font-medium text-base font-poppins">
-                        Lorem ipsum is simply
-                    </p>
-                    <p className="font-medium text-base mt-3 hidden xl:block">
-                        If you don’t have an account register
+                    <h1 className="text-xl font-semibold">Register</h1>
+                    <p className="font-medium text-base mt-3">
+                        If you already have an account
                         <br />
                         You can{" "}
                         <a href="#" className="text-red-600">
-                            Register here!
+                            Login here!
                         </a>
                     </p>
                 </div>
@@ -74,6 +74,38 @@ const Login = () => {
                                 placeholder="Enter your email address"
                             />
                         </div>
+                    </div>
+                    <div className="relative">
+                        <label
+                            htmlFor="username"
+                            className="block text-sm font-medium text-black"
+                        >
+                            Username
+                        </label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            required
+                            className="w-full pl-3 pr-3 py-2 border-b-2 border-black rounded-none focus:outline-none focus:border-[#FF432A]"
+                            placeholder="Enter your username"
+                        />
+                    </div>
+                    <div className="relative">
+                        <label
+                            htmlFor="phone"
+                            className="block text-sm font-medium text-black"
+                        >
+                            Phone
+                        </label>
+                        <input
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            required
+                            className="w-full pl-3 pr-3 py-2 border-b-2 border-black rounded-none focus:outline-none focus:border-[#FF432A]"
+                            placeholder="Enter your phone number"
+                        />
                     </div>
                     <div className="relative">
                         <label
@@ -108,28 +140,39 @@ const Login = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="flex items-start justify-between mt-2">
-                        <div className="flex items-center">
-                            <input
-                                type="checkbox"
-                                id="remember_me"
-                                name="remember_me"
-                                className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+                    <div className="relative">
+                        <label
+                            htmlFor="confirm_password"
+                            className="block text-sm font-medium text-black"
+                        >
+                            Confirm Password
+                        </label>
+                        <div className="flex items-center mt-1 relative">
+                            <img
+                                src={IconLock}
+                                alt="lock icon"
+                                className="absolute left-3 w-4 h-4 text-black"
                             />
-                            <label
-                                htmlFor="remember_me"
-                                className="ml-2 text-sm text-gray-600"
+                            <input
+                                type={
+                                    confirmPasswordVisible ? "text" : "password"
+                                }
+                                id="confirm_password"
+                                name="confirm_password"
+                                required
+                                className="w-full pl-10 pr-10 py-2 border-b-2 border-black rounded-none focus:outline-none focus:border-[#FF432A]"
+                                placeholder="Confirm your password"
+                            />
+                            <div
+                                className="absolute right-3 cursor-pointer"
+                                onClick={toggleConfirmPasswordVisibility}
                             >
-                                Remember me
-                            </label>
-                        </div>
-                        <div className="flex items-start">
-                            <a
-                                href="#"
-                                className="text-sm text-gray-600 hover:underline hover:text-indigo-600"
-                            >
-                                Forgot <br /> Password?
-                            </a>
+                                {confirmPasswordVisible ? (
+                                    <FaEyeSlash className="w-5 h-5 text-black" />
+                                ) : (
+                                    <IoEyeSharp className="w-5 h-5 text-black" />
+                                )}
+                            </div>
                         </div>
                     </div>
                     <div className="mt-4">
@@ -137,18 +180,8 @@ const Login = () => {
                             type="submit"
                             className="w-full px-4 py-3 text-white bg-primary rounded-3xl hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 font-medium font-poppins"
                         >
-                            Login
+                            Register
                         </button>
-                        <div className="font-poppins text-left mt-6 ml-5 xl:hidden">
-                            <p className="font-normal text-sm mt-3">
-                                If you don’t have an account register
-                                <br />
-                                You can{" "}
-                                <a href="#" className="text-indigo-600">
-                                    Register here!
-                                </a>
-                            </p>
-                        </div>
                     </div>
                 </form>
             </div>
@@ -156,4 +189,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
