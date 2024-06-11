290c42f54dbcec306d1e91d2d99139de850e61ab
talitharyn <talitharyn@gmail.com>
2024-06-11 18:27:34 +0700

    fix conflict

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -2,16 +2,16 @@ import { InstagramFilled, WhatsAppOutlined } from "@ant-design/icons";
 
 const Footer = () => {
   const hoverStyle = {
-    color: "#064F3B", // Warna saat di-hover
+    color: "#064F3B",
   };
   return (
-    <footer className="flex flex-col xl:flex-row xl:justify-between text-center xl:text-left py-5 xl:py-24">
+    <footer className="flex flex-col xl:flex-row xl:justify-between text-center xl:text-left py-5 xl:py-16 xl:w-full xl:p-[24px]">
       <div className="text-md mb-4 text-[#9795B5] leading-8">
         <span className="xl:hidden">
           © 2024 Camp Connect <br />
           Hak cipta dilindungi.
         </span>
-        <span className="hidden xl:block">
+        <span className="hidden xl:flex">
           © 2024 Camp Connect | Hak cipta dilindungi.
         </span>
       </div>
