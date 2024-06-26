85aa014ef1bb3c9bcaafa495dde26e5898cc25c9
MRickyyZ <rickykudou@gmail.com>
2024-06-26 11:06:05 +0800

    update dist

---
+++ dist/assets/useAuthContext-Cqf6he8v.js
@@ -0,0 +1,1 @@
+import{r as e,A as o}from"./index-Cjl7ykrD.js";const s=()=>{const t=e.useContext(o);if(!t)throw Error("useAuthContext must be used inside an AuthContextProvider");return t};export{s as u};
